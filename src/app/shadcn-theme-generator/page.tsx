9832493597dc145f11987a6f95aa75cd819e999a
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2024-12-10 22:08:53 +0530

    animations added

--- src/app/shadcn-theme-generator/page.tsx
+++ src/app/shadcn-theme-generator/page.tsx
@@ -5,6 +5,7 @@ import ThemeCustomizer from "@/shadcn-theme-generator/components/theme-customize
 import useThemeStyles from "@/shadcn-theme-generator/hooks/useThemeStyles";
 import { useEffect, useState } from "react";
 
+import { motion } from "framer-motion";
 export default function Home() {
   const [mounted, setMounted] = useState(false);
   useThemeStyles();
@@ -16,9 +17,15 @@ export default function Home() {
   if (!mounted) return null;
 
   return (
-    <div className="container mx-auto py-6 scroll-mt-20 space-y-6 px-4 md:px-8">
+    <div className="container mx-auto py-6 scroll-mt-20 space-y-6 px-4 md:px-8 pt-20">
       <ThemeCustomizer />
-      <CardsDemo />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.6 }}
+      >
+        <CardsDemo />
+      </motion.div>
     </div>
   );
 }
--- src/shadcn-theme-generator/components/theme-customizer/theme-customizer.tsx
+++ src/shadcn-theme-generator/components/theme-customizer/theme-customizer.tsx
@@ -6,6 +6,7 @@ import AppearanceToggle from "./appearance-toggle";
 import { useTheme } from "next-themes";
 import ColorPalette from "./color-palette";
 
+import { motion } from "framer-motion";
 type Props = {};
 
 export default function ThemeCustomizer({}: Props) {
@@ -19,20 +20,47 @@ export default function ThemeCustomizer({}: Props) {
     <div className="flex flex-col items-center justify-center py-6">
       <div className="py-8">
         <div className="text-center font-calSans">
-          <h2 className="text-2xl ">shadcn/ui</h2>
-          <h1 className="text-5xl bg-foreground bg-gradient-to-tl from-primary/80 via-foreground to-foreground bg-clip-text text-transparent">
+          <motion.h2
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7, delay: 0.1 }}
+            className="text-2xl "
+          >
+            shadcn/ui
+          </motion.h2>
+          <motion.h1
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="text-5xl bg-foreground bg-gradient-to-tl from-primary/80 via-foreground to-foreground bg-clip-text text-transparent"
+          >
             Personalize Your Theme
-          </h1>
+          </motion.h1>
         </div>
-        <p className="font-geist text-sm my-4 text-foreground/50 text-center">
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className="font-geist text-sm my-1 text-foreground/50 text-center"
+        >
           Generate your theme and personalize it to your liking.
-        </p>
+        </motion.p>
       </div>
 
-      <div className="flex items-center justify-center my-6">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.3 }}
+        className="flex items-center justify-center my-6"
+      >
         <AppearanceToggle />
-      </div>
-      <div className="flex gap-4 my-6">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7, delay: 0.4 }}
+        className="flex gap-4 my-6"
+      >
         <ColorInput
           identifier={`${appearance}/primary`}
           label="Primary Color"
@@ -42,10 +70,15 @@ export default function ThemeCustomizer({}: Props) {
           identifier={`${appearance}/background`}
           label="Background Color"
         />
-      </div>
-      <div className="my-8">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.5 }}
+        className="my-8"
+      >
         <ColorPalette />
-      </div>
+      </motion.div>
     </div>
   );
 }
